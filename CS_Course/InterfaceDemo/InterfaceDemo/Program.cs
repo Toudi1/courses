379e214f4c61d6e7fd1f42a7cb734f91abc903cd
Toudi1 <b.tomek0@gmail.com>
2023-03-18 20:25:09 +0100

    interfaceDemo

---
+++ CS_Course/InterfaceDemo/InterfaceDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using InterfaceDemo;
+
+Ticket t1 = new Ticket(12);
+Ticket t2 = new Ticket(10);
+Ticket t3 = new Ticket(12);
+
+Console.WriteLine(t1.Equals(t2));
+Console.WriteLine(t1.Equals(t3));
+
+Console.ReadKey();
