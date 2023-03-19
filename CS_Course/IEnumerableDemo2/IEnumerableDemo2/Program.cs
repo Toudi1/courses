14a004feff3e91e5694fd45c6b69d903ac52376f
Toudi1 <b.tomek0@gmail.com>
2023-03-19 10:14:51 +0100

    IEnumerableDemo2

---
+++ CS_Course/IEnumerableDemo2/IEnumerableDemo2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> numberList = new List<int>() { 9, 5, 13 };
+
+        int[] numberArray = new int[] { 13, 15, 3, 7, 2 };
+
+        Console.WriteLine("  ");
+
+        CollectionSum(numberList);
+        Console.WriteLine("  ");
+        CollectionSum(numberArray);
+
+
+    }
+    static void CollectionSum(IEnumerable<int> anyCollection)
+    {
+        int sum = 0;
+
+        foreach (int num in anyCollection)
+        {
+            sum += num;
+        }
+        Console.WriteLine("The sum is: " + sum);
+    }
+}
