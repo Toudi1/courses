5b1afe529f822553da503cc930d705a43b6f55f9
Toudi1 <b.tomek0@gmail.com>
2023-03-22 19:35:58 +0100

    FileReader

---
+++ CS_Course/FileOc/FileOc/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//example 1
+string text = System.IO.File.ReadAllText(@"D:\programowanie\GitCourses\courses\CS_Course\Sources\readTxt\ExampleTekst.txt");
+
+
+
+Console.WriteLine("Whole tekst id: {0}",text);
+
+string[] lines = System.IO.File.ReadAllLines(@"D:\programowanie\GitCourses\courses\CS_Course\Sources\readTxt\ExampleTekst.txt");
+
+
+Console.WriteLine("Line reader");
+
+foreach (string lineTxt in lines)
+{
+    Console.WriteLine("\t" + lineTxt);
+
+}
+
+Console.ReadKey();
