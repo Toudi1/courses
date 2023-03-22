8a70f1bd5dcdb7f681f73cd769f15e7d05b32074
Toudi1 <b.tomek0@gmail.com>
2023-03-22 20:50:31 +0100

    ParsingGameTask1

---
+++ CS_Course/ParsingGame/ParsingGame/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+using System.Runtime.CompilerServices;
+
+string[] lines = System.IO.File.ReadAllLines(@"D:\programowanie\GitCourses\courses\CS_Course\Sources\readTxt\input.txt");
+
+using (StreamWriter file = new StreamWriter(@"D:\programowanie\GitCourses\courses\CS_Course\Sources\readTxt\output.txt", true))
+{
+	foreach (string line in lines)
+	{
+		if (line.Contains("split"))
+		{
+			string[] splited = line.Split();
+
+            file.Write(splited[4] + " ");
+        }
+	}
+}
+
+
+Console.ReadKey();
