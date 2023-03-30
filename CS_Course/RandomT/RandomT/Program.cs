2d16ab562759d81e25eba78b37e868f95ba92c35
Toudi1 <b.tomek0@gmail.com>
2023-03-30 20:05:32 +0200

    Random

---
+++ CS_Course/RandomT/RandomT/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+Random dice = new Random();
+int numEyes;
+
+for (int i = 0; i < 10; i++)
+{
+    numEyes = dice.Next(1, 7);
+    Console.WriteLine(numEyes);
+}
+
+Console.WriteLine("Please enter your question:");
+Console.ReadLine();
+Answer(dice.Next(1, 4));
+
+
+Console.ReadKey();
+
+
+ void Answer(int num)
+{
+    switch (num)
+    {
+        case 1:
+            Console.WriteLine("Yes");
+            break;
+        case 2:
+            Console.WriteLine("Maby");
+            break;
+        case 3:
+            Console.WriteLine("No");
+            break;
+
+        default:
+            Console.WriteLine("Something went wrong!");
+            break;
+    }
+}
