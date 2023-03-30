5defa0151f2b8743867805088d8f52863314ffff
Toudi1 <b.tomek0@gmail.com>
2023-03-30 20:51:32 +0200

    RegularExpresions

---
+++ CS_Course/RegexE/RegexE/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+using System.Text.RegularExpressions;
+
+string pattern = @"\d";
+
+
+Regex regex = new Regex(pattern);
+
+string text = "Hi ther, my number is 135881";
+
+MatchCollection matchCollection = regex.Matches(text);
+
+Console.WriteLine("{0} hits found:\n {1}",matchCollection.Count,text);
+
+foreach (Match hit in matchCollection)
+{
+    GroupCollection group = hit.Groups;
+    Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+}
+
+
+Console.ReadKey();
+
