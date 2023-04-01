0754b871dc108988ba8d4fedb66b37263f93135d
Toudi1 <b.tomek0@gmail.com>
2023-04-01 16:52:24 +0200

    MainArgsDemo

---
+++ CS_Course/MainArgsDemo/MainArgsDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿
+if (args.Length == 0)
+{
+    Console.WriteLine("Please provide arguments");
+    Console.ReadKey();
+    return;
+}
+Console.WriteLine("Hello " + args[0]);
+Console.ReadKey();
+
