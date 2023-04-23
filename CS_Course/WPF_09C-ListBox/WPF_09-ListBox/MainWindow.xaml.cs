350f3e5ca6fe6f3c55f3830476f4d669dc2fdb10
Toudi1 <b.tomek0@gmail.com>
2023-04-23 21:20:09 +0200

    WPF_09C-ListBox

---
+++ CS_Course/WPF_09C-ListBox/WPF_09-ListBox/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_09_ListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match() { Team1 = "Jagiellonia Białystok", Team2 = "Raków Częstochowa", Score1 = 1, Score2 = 3, Completion = 80 });
+            matches.Add(new Match() { Team1 = "Legia", Team2 = "Lech", Score1 = 2, Score2 = 2, Completion = 50 });
+
+            lbMatches.ItemsSource = matches;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbMatches.SelectedItem != null)
+            {
+                MessageBox.Show("Selected match: " +
+                    (lbMatches.SelectedItem as Match).Team1 + " " +
+                    (lbMatches.SelectedItem as Match).Score1 + " " +
+                    (lbMatches.SelectedItem as Match).Score2 + " " +
+                    (lbMatches.SelectedItem as Match).Team2);
+            }
+        }
+    }
+
+
+
+
+    public class Match
+    {
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Completion { get; set; }
+    }
+
+
+}
