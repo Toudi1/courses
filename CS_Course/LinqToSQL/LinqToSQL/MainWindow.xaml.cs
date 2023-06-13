5e04d2c5eabcf759e83b2653d8665a4679b42ee7
Toudi1 <b.tomek0@gmail.com>
2023-06-13 23:02:19 +0200

    addingSQLDataByProgram

--- CS_Course/LinqToSQL/LinqToSQL/MainWindow.xaml.cs
+++ CS_Course/LinqToSQL/LinqToSQL/MainWindow.xaml.cs
@@ -31,6 +31,44 @@ namespace LinqToSQL
 
             string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.TutorialDBConnectionString"].ConnectionString;
             dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+            InsertUniversities();
+            InsertStudent();
+        }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            University yale = new University();
+            yale.Name = "Yale";
+            dataContext.Universities.InsertOnSubmit(yale);
+
+            University oxford = new University();
+            oxford.Name = "Oxford";
+            dataContext.Universities.InsertOnSubmit(oxford);
+
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+        public void InsertStudent()
+        {
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale"));
+            University oxford = dataContext.Universities.First(un => un.Name.Equals("Oxford"));
+            dataContext.ExecuteCommand("delete from Student");
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "John", Gender = "male", UniversityId = yale.Id });
+            students.Add(new Student { Name = "Carla", Gender = "female", University = yale});
+            students.Add(new Student { Name = "Jame", Gender = "male", University = oxford});
+            students.Add(new Student { Name = "Leyla", Gender = "female", University = oxford});
+            students.Add(new Student { Name = "Toni", Gender = "male", University = yale});
+
+            dataContext.Students.InsertAllOnSubmit(students);
+
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Students;
         }
     }
 }
