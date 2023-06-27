03ca87b47b164c661af915d50ee4a039e24c62da
Toudi1 <b.tomek0@gmail.com>
2023-06-27 22:22:40 +0200

    Exercixe2

---
+++ CS_Course/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers1 = { 1, 1, 1, 2, 3, 4, 5, 1, 2 };
+        SumOfTwo(numbers1, 2);
+
+        Console.ReadKey();
+
+    }
+    public static int SumOfTwo(int[] nums, int SumToFind)
+    {
+        int numberOfPairs = 0;
+        int firstValue = 0;
+        int secondValue = 0;
+        int rest = 0;
+        Array.Sort(nums);
+        List<int> collection = new List<int>(nums);
+        int index = collection.IndexOf(SumToFind);
+        
+        while (collection.First() < SumToFind)
+        {
+            firstValue = collection.First();
+            collection.Remove(firstValue);
+            rest = SumToFind - firstValue;
+            try
+            {
+                index = collection.IndexOf(rest);
+                secondValue = collection.ElementAt(index);
+                collection.RemoveAt(index);
+                numberOfPairs++;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine(numberOfPairs);
+        return numberOfPairs;
+    }
+}
