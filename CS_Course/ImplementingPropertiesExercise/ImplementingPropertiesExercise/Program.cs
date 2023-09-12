5ba29815394ac10fb6e9461aebca57257924a96a
Toudi1 <b.tomek0@gmail.com>
2023-09-12 20:11:19 +0200

    WPFDemo

---
+++ CS_Course/ImplementingPropertiesExercise/ImplementingPropertiesExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Book book;
+        book("title",50);
+        string title = book._title;
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Book
+{
+    public string _title { get; set; } = "Unknown";
+    public int _pages { get; set; } = 1;
+    public Book(string Title, int Pages)
+    {
+        _title = Title;
+        if (Pages < 1) { _pages = Pages; }else { _pages = 1; }
+    }
+}
